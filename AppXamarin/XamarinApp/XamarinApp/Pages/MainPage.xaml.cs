c85245a61ea1eb3ae1eb9e960295521bb5e30799
Jose Manuel Alvarez Cervantes <a1217234@uabc.edu.mx>
2016-05-23 14:46:14 -0700

    Otro commit

--- AppXamarin/XamarinApp/XamarinApp/Pages/MainPage.xaml.cs
+++ AppXamarin/XamarinApp/XamarinApp/Pages/MainPage.xaml.cs
@@ -65,7 +65,7 @@ namespace XamarinApp.Pages
         }
 
         public async void OnItemSelected(object sender, EventArgs e)
-        {
+        {            
                  var detailPage = new InformationPageModal();
 
                  await Navigation.PushModalAsync(detailPage);
