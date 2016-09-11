97f04078b36583b307e865f28ebfbb9ec83722c7
Jose Manuel <alvarez.m23@hotmail.com>
2016-09-11 16:28:01 -0700

    Notification Business added

--- AppXamarin/XamarinApp/XamarinApp/Pages/ConfigurationPage.xaml.cs
+++ AppXamarin/XamarinApp/XamarinApp/Pages/ConfigurationPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using XamarinApp.Services;
 
 namespace XamarinApp.Pages
 {
@@ -19,7 +20,15 @@ namespace XamarinApp.Pages
             bool isToggled = e.Value;
 
             if (isToggled)
+            {
+                DependencyService.Get<ILocalNotifications>().SendLocalNotification(
+                    "Notification title",
+                    "Notification content / description",
+                    0
+                );
                 await DisplayAlert("Atención", "Las notificaciones se han activado", "Entendido");
+
+            }
             else
                 await DisplayAlert("Atención", "Las notificaciones se han desactivado", "Entendido");
 
---
+++ AppXamarin/XamarinApp/XamarinApp/Services/ILocalNotifications.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamarinApp.Services
+{
+    public interface ILocalNotifications
+    {
+        void SendLocalNotification(string title, string description, int iconId);
+    }
+}
